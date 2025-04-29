ad3a62d435b79ba23898ded028a94d3a7765d310
philling83 <philipling83@gmail.com>
2025-04-29 19:21:12 -0400

    Clean up

--- 30-Week/1-Day/aa15-thunk-action-creator/frontend/src/components/SingleArticle/SingleArticle.jsx
+++ 30-Week/1-Day/aa15-thunk-action-creator/frontend/src/components/SingleArticle/SingleArticle.jsx
@@ -5,23 +5,21 @@ import './SingleArticle.css';
 const SingleArticle = () => {
   const { id } = useParams();
   const singleArticle = useSelector(
-    state => state.articleState.entries.find(article => article.id === +id)
-  )
-
-  console.log("SINGLE ARTICLE", singleArticle)
-
-  return (
-    <div className='singleArticle'>
-      <h1>{singleArticle?.title}</h1>
-      <img
-        src={singleArticle?.imageUrl}
-        alt={singleArticle?.title}
-      />
-      <p>
-        {singleArticle?.body}
-      </p>
-    </div>
+    state => state.articleState.entries.find(article => article.id === +id);
   );
+
+return (
+  <div className='singleArticle'>
+    <h1>{singleArticle?.title}</h1>
+    <img
+      src={singleArticle?.imageUrl}
+      alt={singleArticle?.title}
+    />
+    <p>
+      {singleArticle?.body}
+    </p>
+  </div>
+);
 };
 
 export default SingleArticle;
